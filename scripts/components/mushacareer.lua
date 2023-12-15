7fb4afcdbf299b8eecde946b1457bdf480e5d305
PixelNeko <160456897+PxNeko@users.noreply.github.com>
2023-12-14 23:18:53 -0600

    Update mushacareer

--- scripts/components/mushacareer.lua
+++ scripts/components/mushacareer.lua
@@ -1,12 +1,22 @@
 local VALID_STRUCTRUES = {
     "researchlab2",
-    "researchlab4",
+    "researchlab3",
+}
+
+local BONUS_DAYS = {
+    1,4,10,20,30,40,50,60,80,100,120,140,160,180,200,220,240,
+    260,280,300,320,340,360,380,400,420,440,460,480,500
 }
 
 local MushaCareer = Class(function(self, inst)
     self.inst = inst
     self.killcounter = {}
     self.buildcounter = {}
+    self.bonusdays = BONUS_DAYS
+
+    self.inst:ListenForEvent("cycleschanged", function(_, cycle)
+        self:OnCyclesChanged(cycle)
+    end, TheWorld)
 
     self.inst:ListenForEvent("entity_death", function(_, data)
         self:OnEntityDeath(data)
@@ -21,16 +31,27 @@ function MushaCareer:OnSave()
     return {
         killcounter = self.killcounter,
         buildcounter = self.buildcounter,
+        bonusdays = self.bonusdays,
     }
 end
 
 function MushaCareer:OnLoad(data)
     self.killcounter = data.killcounter or {}
     self.buildcounter = data.buildcounter or {}
+    self.bonusdays = data.bonusdays or BONUS_DAYS
+end
+
+function MushaCareer:OnCyclesChanged(cycle)
+    if self.inst.components.age and table.contains(self.bonusdays, self.inst.components.age:GetAgeInDays()) then
+        table.removearrayvalue(self.bonusdays, self.inst.components.age:GetAgeInDays())
+        if self.inst.components.mushaskilltree then
+            self.inst.components.mushaskilltree:AddSkillXP(1)
+        end
+    end
 end
 
 function MushaCareer:OnEntityDeath(data)
-    if self.inst.components.health:IsDead() or inst:HasTag("playerghost") then
+    if self.inst.components.health:IsDead() or self.inst:HasTag("playerghost") then
         return
     end
 
